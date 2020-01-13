994b6ddbe867096a1db2a5a567548ee1d9758577
MinThetMaung <minthetmaung96@gmail.com>
2020-01-13 23:55:14 +0630

    Initial project setup

---
+++ goalpost/Controllers/GoalsVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  goalpost
+//
+//  Created by Min Thet Maung on 13/01/2020.
+//  Copyright © 2020 Myanmy. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func addGoalBtnPressed(_ sender: Any) {
+    }
+}
+
